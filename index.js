69291efbad4225fbfc767b085d002067b391a01c
BHAVANIBOJEDLA <bhavanibojadla8@gmail.com>
2024-07-09 13:50:33 +0530

    your message

--- index.js
+++ index.js
@@ -1,38 +1,36 @@
-const express = require('express')
-const mongoose=require("mongoose");
-const cors=require("cors");
-const app = express()
-const userRoutes=require("./routes/userRoutes");
-const incomesRoutes=require("./routes/incomesRoutes");
-const expenditureRoutes=require("./routes/expenditureRoutes");
-const dbConnect=async()=>{
-    try{
-       await mongoose.connect("mongodb://localhost:27017/FinanceTracker",
-        //  { useNewUrlParser: true, useUnifiedTopology: true }
-       );
-       console.log("Database Connected");
-    }
-    catch(e){
-        console.log("error in db connection")
-        console.log(e);
-    }
-} 
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const userRoutes = require("./routes/userRoutes");
+const incomesRoutes = require("./routes/incomesRoutes");
+const expenditureRoutes = require("./routes/expenditureRoutes");
+const dbConnect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://Bhavani-Bojedla:Bhavani123@cluster1.34vtstj.mongodb.net/FinanceTracker"
+      //  { useNewUrlParser: true, useUnifiedTopology: true }
+    );
+    console.log("Database Connected");
+  } catch (e) {
+    console.log("error in db connection");
+    console.log(e);
+  }
+};
 app.use(express.json());
 app.use(cors());
 
-app.use("/expenditure",expenditureRoutes);
-app.use("/income",incomesRoutes);
-app.use("/users",userRoutes);
+app.use("/expenditure", expenditureRoutes);
+app.use("/income", incomesRoutes);
+app.use("/users", userRoutes);
 
-app.get('/', (req, res) => {
-    res.send("hello finance")
+app.get("/", (req, res) => {
+  res.send("hello finance");
 });
 app.listen(4000, () => {
-    dbConnect();
-    console.log(`Server is Listening on 4000`)
+  dbConnect();
+  console.log(`Server is Listening on 4000`);
 });
-
-
 
 // "body-parser": "^1.20.2",
 // "cors": "^2.8.5",
